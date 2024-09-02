2ff5e3ddce176848735e37194cfe930f1bbffc1f
Riley de Man <rileydeman@gmail.com>
2024-09-02 10:53:59 +0200

    Small changes to home.php
    Small changes to home.php

--- public/home.php
+++ public/home.php
@@ -10,15 +10,15 @@ include_once("app/config/config.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HOME | siteName</title>
+    <title>HOME | AnnexBios Leidscherijn</title>
 
     <link rel="icon" href="">
 
 <!--    Linking of the css files-->
-    <link rel="stylesheet" href="/public/assets/css/style.css">
+    <link rel="stylesheet" href="<?= BASEURL ?>public/assets/css/style.css">
 
 <!--    Linking of the javascript files, defer = reading/link the file at last-->
-    <script defer src="/public/assets/js/app.js"></script>
+    <script defer src="<?= BASEURL ?>public/assets/js/app.js"></script>
 </head>
 
 <body>
