31a692da63b055e427162aa8b125e349d16b70b7
Riley de Man <rileydeman@gmail.com>
2024-09-03 16:14:50 +0200

    Header almost ready
    Header almost ready
    
    To do for header:
    Select box better styling
    Button for form submission

--- public/core/header.php
+++ public/core/header.php
@@ -1,2 +1,30 @@
-<!--Create the header without the header tags-->
-<p>Header</p>
+<div id="head">
+    <div id="left">
+        <a href="<?= BASEURL ?>" id="logo">
+            <img src="<?= BASEURL ?>public/assets/img/AnnexBios-leidscherijn-logo.png" alt="">
+        </a>
+    </div>
+    <nav>
+        <?php
+        $nav = file_get_contents(BASEURL . "public/assets/json/nav.json");
+        $nav = json_decode($nav, true);
+
+        foreach ($nav as $navItem) {
+        ?>
+        <a href="<?= $navItem["url"] ?>" class="menuItem">
+            <p><?= strtoupper($navItem["name"]) ?></p>
+        </a>
+        <?php } ?>
+    </nav>
+</div>
+
+<div id="bottom">
+    <form action="">
+        <p id="formTitle">KOOP JE TICKETS</p>
+        <select name="film" id="">
+            <option value="hideOption">Kies je film</option>
+            <option value="">Jurrassic World: Rebirth</option>
+            <option value="">Insidious: The Red Door</option>
+        </select>
+    </form>
+</div>
