84168c040ad5569e02f95821e890e1011d77e513
Riley de Man <rileydeman@gmail.com>
2024-09-03 15:36:02 +0200

    Added all the needed files
    Added all the needed files:
    -php files
    -css files
    -favicons
    -img
    -social icons
    -kijkwijzer icons

--- public/films-overview.php
+++ public/films-overview.php
@@ -1,6 +1,6 @@
 <?php
 //Including config file
-include_once("app/config/config.php");
+include_once("../app/config/config.php");
 ?>
 
 <!doctype html>
@@ -10,37 +10,37 @@ include_once("app/config/config.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HOME | AnnexBios Leidscherijn</title>
+    <title>Films Overview | AnnexBios Leidscherijn</title>
 
     <link rel="icon" href="<?= BASEURL ?>favicon.ico">
     <link rel="icon" href="<?= BASEURL ?>/favicon.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="<?= BASEURL ?>/favicon.png">
 
-<!--    Linking of the css files-->
+    <!--    Linking of the css files-->
     <link rel="stylesheet" href="<?= BASEURL ?>public/assets/css/fonts.css">
     <link rel="stylesheet" href="<?= BASEURL ?>public/assets/css/style.css">
     <link rel="stylesheet" href="<?= BASEURL ?>public/assets/css/header.css">
     <link rel="stylesheet" href="<?= BASEURL ?>public/assets/css/footer.css">
-    <link rel="stylesheet" href="<?= BASEURL ?>public/assets/css/home.css">
+    <link rel="stylesheet" href="<?= BASEURL ?>public/assets/css/film-overview.css">
 
-<!--    Linking of the javascript files, defer = reading/link the file at last-->
+    <!--    Linking of the javascript files, defer = reading/link the file at last-->
     <script defer src="<?= BASEURL ?>public/assets/js/app.js"></script>
 </head>
 
 <body>
 
 <header>
-<!--    Getting the header document-->
-    <?php include_once("public/core/header.php"); ?>
+    <!--    Getting the header document-->
+    <?php include_once("core/header.php"); ?>
 </header>
 
 <main>
-    <p>Main</p>
+    <p>Main films overview</p>
 </main>
 
 <footer>
-<!--    Getting the footer document-->
-    <?php include_once("public/core/footer.php"); ?>
+    <!--    Getting the footer document-->
+    <?php include_once("core/footer.php"); ?>
 </footer>
 
 </body>
