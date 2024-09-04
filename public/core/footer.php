e426a4192e07dfbd71940f47c34b51f71f0cae66
Riley de Man <rileydeman@gmail.com>
2024-09-04 15:56:35 +0200

    Footer -> info + img
    Footer -> info + img

--- public/core/footer.php
+++ public/core/footer.php
@@ -1,2 +1,27 @@
-<!--Create the footer without the footer tags-->
-<p>Footer</p>
+<div id="top">
+    <div id="left">
+        <div id="info">
+            <img src="<?= BASEURL ?>public/assets/img/AnnexBios-logo.png" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetuer <br> adipiscing elit. Aenean commodo ligula <br> eget dolor. Aenean massa. Cum sociis
+                <br> natoque penatibus et magnis dis parturient <br> montes, nascetur ridiculus mus. Donec quam felis,</p>
+            <a href="" id="footerButtonA">
+                <div id="footerButton">
+                    <p>LEES MEER</p>
+                </div>
+            </a>
+        </div>
+        <div id="navigation">
+            <p>Navigation</p>
+        </div>
+        <div id="socials">
+            <p>Socials</p>
+        </div>
+    </div>
+    <div id="right">
+        <img src="<?= BASEURL ?>public/assets/img/footer-filmrol.png" alt="">
+    </div>
+</div>
+
+<div id="bottom">
+    <p>Bottom</p>
+</div>
